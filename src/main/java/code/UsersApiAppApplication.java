8627bc8d364c961842d6e0dd9082383cff39b9dd
arohith <rohithallu.imail@gmail.com>
2024-02-13 11:56:32 +0530

    configured swagger

--- src/main/java/code/UsersApiAppApplication.java
+++ src/main/java/code/UsersApiAppApplication.java
@@ -2,7 +2,6 @@ package code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class UsersApiAppApplication {
 	public static void main(String[] args) {
